4f0b7391ca29d16c2a31087949dc013b083a5295
JakGradey <42744715+JakGradey@users.noreply.github.com>
2018-09-04 10:35:04 -0500

    I debugged Swat The Bugs program

---
+++ Debug Swat The Bugs/Debug Swat The Bugs/Source.cpp
@@ -0,0 +1,17 @@
+//Debug Swat The Bugs
+//Created/revised by Jacob Grady on 9/4/2018
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double temp = 35.3;
+
+	temp = temp + 1.5;
+
+		cout << "After adding 1.5, the temp variable now contains " << temp << "." << endl;
+
+	system("pause");
+	return 0;
+}
