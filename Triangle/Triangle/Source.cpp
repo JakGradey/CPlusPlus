271a338090b41699d77965dbea81059920d1b629
JakGradey <42744715+JakGradey@users.noreply.github.com>
2018-09-18 10:24:54 -0500

    Triangle

--- Triangle/Triangle/Source.cpp
+++ Triangle/Triangle/Source.cpp
@@ -26,8 +26,8 @@ int perimeter() {
 	cout << "What is the number of your third side? : " << endl;
 	cin >> perimeterThird;
 
-	cout << perimeterFirst << " + " << perimeterSecond << " + " << perimeter << " = " << perimeterFirst + perimeterSecond + perimeterThird << endl;
-
+	cout << perimeterFirst << " + " << perimeterSecond << " + " << perimeterThird  << " = " << perimeterFirst + perimeterSecond + perimeterThird << endl;
+	cout << "" << endl;
 	return 0;
 }
 
@@ -38,6 +38,7 @@ int area() {
 	cout << "What is the base of the triangle? : " << endl;
 	cin >> areaBase;
 	cout << areaHeight << " * " << areaBase << " / 2 = " << areaHeight * areaBase / 2 << endl;
+	cout << "" << endl;
 
 	return 0;
 }
@@ -53,30 +54,33 @@ int rightTriSide() {
 
 	if (sideMissing == 1)
 	{
-		cout << "What is side C?";
+		cout << "What is side C? : ";
 		cin >> c;
 
-		cout << "What is side B";
+		cout << "What is side B? : ";
 		cin >> b;
 		cout << c << " - " << b << " = " << c - b << endl;
+		cout << "" << endl;
 
 	} else if (sideMissing == 2)
 	{
-		cout << "What is side C?";
+		cout << "What is side C? : ";
 		cin >> c;
 
-		cout << "What is side A";
+		cout << "What is side A? : ";
 		cin >> a;
 		cout << c << " - " << a << " = " << c - a << endl;
+		cout << "" << endl;
 
 	}else if (sideMissing == 3)
 	{
-		cout << "What is side A?";
+		cout << "What is side A? : ";
 		cin >> a;
 
-		cout << "What is side B";
+		cout << "What is side B? : ";
 		cin >> b;
 		cout << a << " + " << b << " = " << a + b << endl;
+		cout << "" << endl;
 
 	}
 	return 0;
@@ -84,40 +88,42 @@ int rightTriSide() {
 
 int isocelesTriSide() {
 	double a, b, c;
-	cout << "Which side are you missing in the pythagorean theorem? ";
-	cout << "Side A\nSide B\nSide C" << endl;
-	cout << "Side A = leg, Side B = leg, Side C = hypotenuse" << endl;
+	cout << "Which side are you missing in the pythagorean theorem? " << endl;
+	cout << "Side A = leg\nSide B = leg\nSide C = hypotenuse" << endl;
 	cout << "1 - Side A\n2 - Side B\n3 - Side C" << endl;
 	cin >> sideMissing;
 
 	if (sideMissing == 1)
 	{
-		cout << "What is side C?";
+		cout << "What is side C? : ";
 		cin >> c;
 
-		cout << "What is side B";
+		cout << "What is side B? : ";
 		cin >> b;
 		cout << c << " - " << b << " = " << c - b << endl;
+		cout << "" << endl;
 
 	}
 	else if (sideMissing == 2)
 	{
-		cout << "What is side C?";
+		cout << "What is side C? : ";
 		cin >> c;
 
-		cout << "What is side A";
+		cout << "What is side A? : " << endl;
 		cin >> a;
 		cout << c << " - " << a << " = " << c - a << endl;
+		cout << "" << endl;
 
 	}
 	else if (sideMissing == 3)
 	{
-		cout << "What is side A?";
+		cout << "What is side A? : ";
 		cin >> a;
 
-		cout << "What is side B";
+		cout << "What is side B? : ";
 		cin >> b;
 		cout << a << " + " << b << " = " << a + b << endl;
+		cout << "" << endl;
 
 	}
 	return 0;
@@ -128,13 +134,13 @@ int equilateralTriSide() {
 	cout << "What is the number for one of your sides? : " << endl;
 	cin >> sameNum;
 	cout << "Your missing side is " << sameNum << endl;
-
+	cout << "" << endl;
 	return 0;
 }
 
 int rightTriangle() {
 	cout << "What are you solving for?" << endl;
-	cout << "1 - Perimeter\n2 - Missing Side\n3 - Area\n4 - Leave" << endl;
+	cout << "1 - Perimeter\n2 - Missing Side\n3 - Area" << endl;
 	cin >> solvingFor;
 
 	if (solvingFor == 1) {
@@ -192,26 +198,24 @@ int equilateral() {
 
 int main()
 {
+	do {
+		cout << "Welcome to the triangle calculator. " << endl;
+		cout << "What type of triangle are you solving for? " << endl;
+		cout << "1 - Right Triangle\n2 - Isoceles\n3 - Equilateral\n4 - Leave" << endl;
+		cin >> choosingTriangle;
 
-	cout << "Welcome to the triangle calculator. " << endl;
-	cout << "What type of triangle are you solving for? " << endl;
-	cout << "1 - Right Triangle\n2 - Isoceles\n3 - Equilateral\n4 - Leave" << endl;
-	cin >> choosingTriangle;
+		if (choosingTriangle == 1) {
+			rightTriangle();
+		}
 
-	if (choosingTriangle == 1) {
-		rightTriangle();
-	}
+		else if (choosingTriangle == 2) {
+			isoceles();
+		}
 
-	else if (choosingTriangle == 2) {
-		isoceles();
-	}
-
-	else if (choosingTriangle == 3) {
-		equilateral();
-	}
-	else
-		cout << "Please pick a number that actually does something. " << endl;
-
+		else if (choosingTriangle == 3) {
+			equilateral();
+		}
+	} while (choosingTriangle != 4);
 		system("pause");
 		return 0;
 }
