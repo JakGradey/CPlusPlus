be617c008ca4916aeea0b3389d9753bfe1cd6612
JakGradey <42744715+JakGradey@users.noreply.github.com>
2018-09-25 10:38:11 -0500

    More random number generating

--- Rand Num Gen/Rand Num Gen/Source.cpp
+++ Rand Num Gen/Rand Num Gen/Source.cpp
@@ -6,11 +6,40 @@
 
 using namespace std;
 
-int randomGame() {
+int easyRandomGame() {
 	int numChoice;
 	int secret;
 	int count = 1;
-	
+
+	srand(time(NULL));
+	secret = rand() % 10 + 1;
+
+	do {
+		cout << "Pick a random number from 1-10: ";
+		cin >> numChoice;
+
+		if (numChoice > secret)
+		{
+			cout << "The number you have chose is too high!, guess again stupid! " << endl;
+			count = count + 1;
+		}
+		else if (numChoice < secret)
+		{
+			cout << "The number you have chose is too low!, guess again stupid! " << endl;
+			count = count + 1;
+		}
+		else
+			cout << "You Win! " << endl;
+
+		return 0;
+	} while (numChoice != secret);
+}
+
+int mediumRandomGame() {
+	int numChoice;
+	int secret;
+	int count = 1;
+
 	srand(time(NULL));
 	secret = rand() % 100 + 1;
 
@@ -18,27 +47,41 @@ int randomGame() {
 		cout << "Pick a random number from 1-100: ";
 		cin >> numChoice;
 
-			if (numChoice > secret) 
-			{
+		if (numChoice > secret)
+		{
 			cout << "The number you have chose is too high!, guess again stupid! " << endl;
 			count = count + 1;
-			}
-			else if (numChoice < secret)
-			{
+		}
+		else if (numChoice < secret)
+		{
 			cout << "The number you have chose is too low!, guess again stupid! " << endl;
-			}
-			else 
+		}
+		else
+			cout << "You Win! " << endl;
 
-		} while (numChoice != secret);
-
-	return 0;
+		return 0;
+	} while (numChoice != secret);
 }
 
 
 
 int main() {
+	int gameChoice;
 
-	randomGame();
+	cout << "Welcome to my random number Generator game, choose 1 - Easy\n2 - Medium\n3 - Hard" << endl;
+	cin >> gameChoice
+		if (gameChoice == 1)
+		{
+			easyRandomGame;
+		}
+		else (gameChoice == 2)
+		{
+			mediumRandomGame;
+		}
+		else (gameChoice == 3)
+		{
+		hardRandomGame;
+		}
 
 	system("pause");
 	return 0;
